7a572ecdabdbef361092a4b1c3cc9f846b2674a8
andrewijaya003 <setiawanandredre@gmail.com>
2022-01-03 21:26:04 +0700

    initial commit

---
+++ js/maps.js
@@ -0,0 +1,50 @@
+var btnEncore = document.getElementById("encore-button-header-second-content")
+var btnOverflow = document.getElementById("overflow-button-header-second-content")
+var btnParty = document.getElementById("party-button-header-second-content")
+var btnPhase = document.getElementById("phase-button-header-second-content")
+var btnRotation = document.getElementById("rotation-button-header-second-content")
+var imgArenas = document.getElementById("arenas-img")
+
+var btnKings = document.getElementById("kings-button-header-second-content")
+var btnWorld = document.getElementById("world-button-header-second-content")
+var btnOlympus = document.getElementById("olympus-button-header-second-content")
+var btnStorm = document.getElementById("storm-button-header-second-content")
+var imgBattle = document.getElementById("battle-royale-img")
+
+function arenasToEncore(){
+    imgArenas.src = "/assets/encore-maps.jpg"
+}
+
+function arenasToOverflow(){
+    imgArenas.src = "/assets/overflow-maps.jpg"
+}
+
+function arenasToParty(){
+    imgArenas.src = "/assets/party-maps.jpg"
+}
+
+function arenasToPhase(){
+    imgArenas.src = "/assets/phase-maps.jpg"
+}
+
+function arenasToRotation(){
+    imgArenas.src = "/assets/rotation-maps.jpg"
+}
+
+function battleToKings(){
+    imgBattle.src = "/assets/kings-maps.jpg"
+}
+
+function battleToWorld(){
+    imgBattle.src = "/assets/world-maps.jpg"
+}
+
+function battleToOlympus(){
+    imgBattle.src = "/assets/olympus-maps.jpg"
+}
+
+function battleToStorm(){
+    imgBattle.src = "/assets/storm-maps.jpg"
+}
+
+
